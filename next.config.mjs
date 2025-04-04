dfe9ae45589b5b7fc1fc2cf13329f5c3ee4c5382
samer <judosamer555@gmail.com>
2025-04-04 19:51:11 +0200

    refactor: clean up and optimize component and config files
    - Standardize indentation and formatting in next.config.mjs
    - Simplify content paths in tailwind.config.ts for better maintainability
    - Replace QuizCard with inline JSX in Subjects.tsx to directly render subject cards with improved styling and hover effects

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
-    images: {
-        domains: ['exam.elevateegy.com','localhost'],
-      },
+  images: {
+    domains: ["exam.elevateegy.com", "localhost"],
+  },
 };
 
 export default nextConfig;
--- src/components/common/Subjects.tsx
+++ src/components/common/Subjects.tsx
@@ -1,9 +1,8 @@
 "use client";
 
 import { useEffect, useState } from "react";
-// import Image from "next/image";
+import Image from "next/image";
 import useSubject from "@/src/hooks/use-subject";
-import QuizCard from "./QuizCard";
 
 function Subjects() {
   const [subjects, setSubjects] = useState<Subject[]>([]);
@@ -19,8 +18,32 @@ function Subjects() {
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
-      {subjects.map((subject) => (
-        <QuizCard key={subject._id} subject={subject} />
+      {subjects.map((subject: Subject) => (
+        <div key={subject._id} className="container rounded-lg w-full">
+          <div className="relative lg:h-[310px] md:h-auto sm:h-auto w-full aspect-video overflow-hidden rounded-lg group">
+            <Image
+              src={subject.icon}
+              alt={subject.name}
+              fill
+              className=" object-cover transition-transform duration-300 group-hover:scale-110"
+            />
+
+            <div className="absolute inset-0 flex items-end">
+              <div className="w-full px-4 pb-4">
+                <div
+                  className="bg-gradient-to-t from-[#332e87] to-[#8181C3] 
+                opacity-90 rounded-lg 
+                w-full
+                p-3
+                text-white 
+                space-y-1">
+                  <h3 className="font-bold text-[15px] truncate">{subject.name}</h3>
+                  <p className="text-[13px] font-medium">Test your skills now!</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       ))}
     </div>
   );

09093b1b1170c58e338ee1684d3b71cb0e64becb
samer <judosamer555@gmail.com>
2025-05-12 07:49:19 +0300

    fix(next.config): update environment variables for auth services
    Update the environment variables in the Next.js configuration to correctly reference `NEXTAUTH_SECRET` and add new variables for Google, Twitter, and Facebook authentication services. This ensures proper configuration and functionality of the authentication system.

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,17 @@ const nextConfig = {
     domains: ["exam.elevateegy.com", "localhost"],
   },
   env: {
-    NEXTAUTH_SECRET: process.env.AUTH_SECRET,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    API: process.env.API,
+
+    AUTH_GOOGLE_ID: process.env.AUTH_GOOGLE_ID,
+    TWITTER_CLIENT_ID: process.env.TWITTER_CLIENT_ID,
+    FACEBOOK_CLIENT_ID: process.env.FACEBOOK_CLIENT_ID,
+
+    AUTH_GOOGLE_SECRET: process.env.AUTH_GOOGLE_SECRET,
+    TWITTER_CLIENT_SECRET: process.env.TWITTER_CLIENT_SECRET,
+    FACEBOOK_CLIENT_SECRET: process.env.FACEBOOK_CLIENT_SECRET,
   },
 };
 

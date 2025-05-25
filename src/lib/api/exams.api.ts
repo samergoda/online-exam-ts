c9ae0686267654ccc5241b21fd292da9418d6900
samer <judosamer555@gmail.com>
2025-05-25 10:43:51 +0300

    refactor(api): replace getToken with getAuthHeader for consistency

--- src/lib/api/exams.api.ts
+++ src/lib/api/exams.api.ts
@@ -1,10 +1,11 @@
-import getToken from "../utils/getToken";
+import getAuthHeader from "../utils/getToken";
 
 export default async function getAllExams() {
-  const token = await getToken();
   const data = await fetch("https://exam.elevateegy.com/api/v1/exams", {
     method: "GET",
-    headers: token ? { token: token } : undefined,
+    headers: {
+      ...(await getAuthHeader()),
+    },
   });
   const res: ExamResponse = await data.json();
   return res;

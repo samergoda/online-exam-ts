7d17f3410f67d79a3e11412f214237d78790a9f1
samer <judosamer555@gmail.com>
2025-05-25 10:41:53 +0300

    refactor(exams): replace getToken with getAuthHeader for consistency

--- src/lib/actions/exams/submit-exam.ts
+++ src/lib/actions/exams/submit-exam.ts
@@ -1,16 +1,15 @@
 "use server";
-import getToken from "../../utils/getToken";
+import getAuthHeader from "../../utils/getToken";
 
 // Submit answers
 export default async function handleSubmitExam(answers: object) {
   // Prevent multiple submissions
-  const token = getToken();
 
   try {
     const response = await fetch("https://exam.elevateegy.com/api/v1/questions/check", {
       method: "POST",
       headers: {
-        token: token,
+        ...(await getAuthHeader()),
         "Content-Type": "application/json",
       },
       body: JSON.stringify(answers),
